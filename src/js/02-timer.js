542f89ac7dd5836d6ca8fb8da925defb6f7124a0
Semestotell <kozlov.k.9527@gmail.com>
2022-08-31 21:27:58 +0200

    1488

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,16 +15,17 @@ let intervalID = null;
 refs.btnStart.disabled = true;
 
 function convertMs(ms) {
-    const ss = 1000;
-    const mm = ss * 60;
-    const hh = mm * 60;
-    const dd = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-    const days = Math.floor(ms / dd);
-    const hours = Math.floor((ms % dd) / hh);
-    const min = Math.floor(((ms % dd) % hh) / mm);
-    const seconds = Math.floor((((ms % dd) % hh) % mm) / ss);
-    return { days, hours, min, seconds };
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
 }
 
 const addLeadingZero = value => String(value).padStart(2, 0);
